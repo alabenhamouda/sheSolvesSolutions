f4c1f4acf514e13e5b4c08ecfd6f1bf93ec419df
alabh <alabenhmouda@gmail.com>
2021-02-21 20:35:48 +0100

    add solution to problem E

---
+++ E. Only One Clue/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int k, x;
+    cin >> k >> x;
+    for (int i = x - k + 1; i <= x + k - 1; i++)
+        cout << i << ' ';
+    return 0;
+}
