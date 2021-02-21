6f7e917eaeab2e1bc453c2541df615420cdff3ba
alabh <alabenhmouda@gmail.com>
2021-02-21 22:40:51 +0100

    Fix comments

--- C. Ons and Factorials/main.cpp
+++ C. Ons and Factorials/main.cpp
@@ -8,9 +8,9 @@ int main() {
     int ret = 0;
     while (n > 0) {
         // count the number of multiples of 5 that smaller or equal to n
+        ret += n / 5;
         // we have to divide n by 5 so that in the next iteration we
         // count the multiples of the next power of 5 (25, 125, ...)
-        ret += n / 5;
         n /= 5;
     }
     cout << ret << endl;
