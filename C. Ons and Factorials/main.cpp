bc5d98b8bfefa8a3e1cd07319ed2e13df5dcf340
alabh <alabenhmouda@gmail.com>
2021-02-21 22:39:55 +0100

    Fix spacing

--- C. Ons and Factorials/main.cpp
+++ C. Ons and Factorials/main.cpp
@@ -7,11 +7,11 @@ int main() {
     cin >> n;
     int ret = 0;
     while (n > 0) {
-        ret +=
-            n /
-            5;  // count the number of multiples of 5 that smaller or equal to n
-        n /= 5; // we have to divide n by 5 so that in the next iteration we
-                // count the multiples of the next power of 5 (25, 125, ...)
+        // count the number of multiples of 5 that smaller or equal to n
+        // we have to divide n by 5 so that in the next iteration we
+        // count the multiples of the next power of 5 (25, 125, ...)
+        ret += n / 5;
+        n /= 5;
     }
     cout << ret << endl;
     return 0;
